2b9e2013fcfb714234bd9cc39e4690fc6f8ff1a5
David <david.ruhe@gmail.com>
2019-04-29 10:09:21 +0200

    update experiments

--- experiments.sh
+++ experiments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-python lvch_2019/train.py --feature-type word_pos --domain Q
-python lvch_2019/train.py --feature-type word_pos --domain N
-python lvch_2019/train.py --feature-type verbal_stem --domain N
-python lvch_2019/train.py --feature-type verbal_stem --domain Q
-python lvch_2019/train.py --feature-type phrase_function --domain N
-python lvch_2019/train.py --feature-type phrase_function --domain Q
+python train.py --feature-type word_pos --domain Q
+python train.py --feature-type word_pos --domain N
+python train.py --feature-type verbal_stem --domain N
+python train.py --feature-type verbal_stem --domain Q
+python train.py --feature-type phrase_function --domain N
+python train.py --feature-type phrase_function --domain Q
